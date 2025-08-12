e8500801e150937655ae47a6073bc3de59188a1f
jvssilva2 <jvssilva2@minha.fag.edu.br>
2025-08-11 22:44:06 -0300

    primeiro comitt projeto web

---
+++ demo/src/main/java/com/example/demo/ProjetospringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringdemoApplication.class, args);
+	}
+
+}
